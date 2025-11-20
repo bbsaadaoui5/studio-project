811aa592b5a9a928f0e9ce13fd497abf4f12f906
zineb <zineb@zinebs-Air.lan>
2025-11-19 20:58:49 -0500

    chore: insert 'use client' where client hooks are used (scripted)

--- src/components/settings/teacher-portal-settings.tsx
+++ src/components/settings/teacher-portal-settings.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState } from "react";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
